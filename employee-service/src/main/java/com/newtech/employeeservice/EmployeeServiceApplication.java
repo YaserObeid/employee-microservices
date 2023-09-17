90672e28d45d8e46c99e8d54f7dae37f31a134d8
Yaser Obeid <yaser.obeid@hotmail.com>
2023-09-17 14:25:33 +0200

    install and define mapstruct bean

--- employee-service/src/main/java/com/newtech/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/newtech/employeeservice/EmployeeServiceApplication.java
@@ -1,13 +1,20 @@
 package com.newtech.employeeservice;
 
+import com.newtech.employeeservice.mapper.MyMapper;
+import org.mapstruct.factory.Mappers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeServiceApplication.class, args);
+	}
+	@Bean
+	public MyMapper myMapper(){
+		return Mappers.getMapper(MyMapper.class);
 	}
 
 }
